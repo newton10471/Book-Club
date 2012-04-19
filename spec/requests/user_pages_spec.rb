98aaec0e8bdc4d6df555c320f1394edfd0276e25
Matt Newton <newton10471@gmail.com>
2012-04-19 05:29:56 -0700

    locking test appears to work

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,23 +15,8 @@ describe "User pages" do
 
     it { should have_selector('title', text: 'All users') }
 
-    # describe "as an admin user" do
-    #   let(:admin) { FactoryGirl.create(:admin) }
-    #   before do
-    #     sign_in admin
-    #     visit users_path
-    #   end
-
-    #   describe "other users should have an unlock link" do
-    #     let!(:locked_user) { FactoryGirl.create(:locked) }
-    #     before { visit users_path }
-
-    #     it { should have_link('unlock', href: user_path(locked_user)) }
-    #   end
-    # end
-
     describe "pagination" do
-      before(:all) { 5.times { FactoryGirl.create(:user) } }
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
       after(:all)  { User.delete_all }
 
       let(:first_page)  { User.paginate(page: 1) }
@@ -67,25 +52,18 @@ describe "User pages" do
         it { should_not have_link('delete', href: user_path(admin)) }
 
         describe "other users should have an unlock link" do  ## changed the description
-          let!(:locked_user) { FactoryGirl.create(:locked) }
-          before { visit users_path }
+          let!(:user) { FactoryGirl.create(:locked) }
+          before do
+            visit users_path
+            click_link "2"
+            # save_and_open_page
+          end
 
-          it { save_and_open_page; should have_link('unlock', href: user_path(locked_user)) }
+          # it { save_and_open_page; should have_link('unlock', href: user_path(locked_user)) }
+          # it { binding.pry; should have_link('unlock', href: user_path(locked_user)) }
+          it { should have_link('unlock', href: unlock_user_path(user)) }
         end
       end
-
-      # describe "as an admin user to unlock new users" do 
-      #   let(:admin) { FactoryGirl.create(:admin) }
-      #   let!(:locked_user) { FactoryGirl.create(:locked) }
-
-      #   before do
-      #     sign_in admin
-      #     visit users_path
-      #   end
-
-      #   before { visit users_path }
-      #   it { binding.pry; should have_link('unlock', href: user_path(locked_user)) }
-      # end
 
       it "should list each user" do
         User.all[0..2].each do |user|
