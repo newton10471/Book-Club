e302424719eab0e1831d722641e0d355235b12c4
Matt Newton <newton10471@gmail.com>
2012-04-20 06:34:43 -0400

    update to Rails 3.2.3

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20120226044448) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           :default => false
-    t.boolean  "locked",          :default => true,  :null => false
+    t.boolean  "locked",          :default => false, :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
